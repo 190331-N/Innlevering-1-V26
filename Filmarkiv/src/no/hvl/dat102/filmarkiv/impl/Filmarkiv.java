b01e9194a5ccf8f9a479192ef28049016fbc3710
HansArne04 <hansarnemid@gmail.com>
2026-01-20 17:07:10 +0100

    Update Filmarkiv.java

--- Filmarkiv/src/no/hvl/dat102/filmarkiv/impl/Filmarkiv.java
+++ Filmarkiv/src/no/hvl/dat102/filmarkiv/impl/Filmarkiv.java
@@ -2,6 +2,8 @@ package no.hvl.dat102.filmarkiv.impl;
 
 import no.hvl.dat102.filmarkiv.adt.FilmarkivADT;
 
+import java.util.Arrays;
+
 public class Filmarkiv implements FilmarkivADT {
 
     private Film[] filmarkiv;
@@ -15,22 +17,48 @@ public class Filmarkiv implements FilmarkivADT {
 
     @Override
     public Film finnFilm(int nr) {
+        for (int i = 0; i < antall; i++) {
+            if (filmarkiv[i].getFilmnr() == nr){
+                return filmarkiv[i];
+            }
+        }
         return null;
     }
 
     @Override
     public void leggTilFilm(Film nyFilm) {
-
+        if (filmarkiv.length >= antall){
+            filmarkiv = Arrays.copyOf(filmarkiv, filmarkiv.length*2);
+        }
+        filmarkiv[antall] = nyFilm;
+        antall++;
     }
 
     @Override
     public boolean slettFilm(int filmnr) {
+        for (int i = 0; i < antall; i++) {
+            if (filmarkiv[i].getFilmnr() == filmnr){
+                filmarkiv[i] = filmarkiv[antall-1];
+                filmarkiv[antall-1] = null;
+                antall--;
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public Film[] soekTittel(String delstreng) {
-        return new Film[0];
+        Film[] resultat = new Film[antall];
+        int funnet = 0;
+
+        for (int i = 0; i < antall; i++) {
+            if (filmarkiv[i].getTittel().contains(delstreng)){
+                resultat[funnet] = filmarkiv[i];
+                funnet++;
+            }
+        }
+        return resultat;
     }
 
     @Override
